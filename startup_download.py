f9c9ae68f15ce5992ac16eecef441517c0b6c7ee
Palash-Modi <modi.palash03@gmail.com>
2025-04-27 07:52:06 +0530

    Fix path for embeddings folder (no /mnt)

--- startup_download.py
+++ startup_download.py
@@ -2,9 +2,12 @@ import os
 import gdown
 
 # Make sure the directory exists
-os.makedirs("/mnt/embeddings", exist_ok=True)
+os.makedirs("embeddings", exist_ok=True)
+output_path = "embeddings/embeddings.pt"
+
+
 
 drive_url = "https://drive.google.com/uc?id=17mi94fCQDxo4ENcqOQ25TN1O3lCE-W25"
-output_path = "/mnt/embeddings/embeddings.pt"
+
 
 gdown.download(drive_url, output_path, quiet=False)
