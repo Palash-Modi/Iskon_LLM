036f382e9d52dd66e5f78f9521be4de45102bd2e
Palash-Modi <modi.palash03@gmail.com>
2025-04-27 08:35:17 +0530

    Hopefully Final

--- startup_download.py
+++ startup_download.py
@@ -1,13 +1,15 @@
 import os
 import gdown
 
-# Make sure the directory exists
-os.makedirs("embeddings", exist_ok=True)
-output_path = "embeddings/embeddings.pt"
+def download_embeddings():
+    # Make sure the directory exists
+    os.makedirs("embeddings", exist_ok=True)
+    output_path = "embeddings/embeddings.pt"
 
+    drive_url = "https://drive.google.com/uc?id=17mi94fCQDxo4ENcqOQ25TN1O3lCE-W25"
 
+    gdown.download(drive_url, output_path, quiet=False)
 
-drive_url = "https://drive.google.com/uc?id=17mi94fCQDxo4ENcqOQ25TN1O3lCE-W25"
-
-
-gdown.download(drive_url, output_path, quiet=False)
+# Optional: Allow running manually too
+if __name__ == "__main__":
+    download_embeddings()
